d722df9ea3edee34a07474db8b340f2d0ebc2c57
Katniss-Nataliia <natko.boiko@gmail.com>
2023-07-30 20:35:37 -0600

    added some code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
+import React from "react";
+import { PhoneBook } from "./phonebook";
+
 export const App = () => {
   return (
     <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
+      // style={{
+      //   height: '100vh',
+      //   display: 'flex',
+      //   justifyContent: 'center',
+      //   alignItems: 'center',
+      //   fontSize: 40,
+      //   color: '#010101'
+      // }}
     >
-      React homework template
+      <PhoneBook/>
     </div>
   );
 };
---
+++ src/components/phonebook.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+import { nanoid } from 'nanoid';
+
+export class PhoneBook extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            contacts: [],
+            name: ''
+        }
+    }
+
+    handleChange = (e) => {
+        return this.setState({name: e.target.value})
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const {name} = this.state;
+        const {addContact} = this.props;
+
+        addContact(name); // store the entered name here?
+
+        this.setState({name:""}); //reverse the input to empty line
+        
+    }
+
+    render() {
+        const{name} = this.state;
+        const {handleChange, handleSubmit} = this
+
+        return (
+            <div>
+                <input
+                    type="text"
+                    name="name"
+                    pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                    title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan."
+                    required
+                    value={name}
+                    onChange = {handleChange}
+                />
+                <button onClick={handleSubmit}>Add contact</button>
+                <h2>contact</h2>
+                <ul>
+                    <li>{name}</li>
+                </ul>
+            </div>
+        )
+    }
+
+}
+
+
