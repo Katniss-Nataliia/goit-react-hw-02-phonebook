beb8908d0ba29450ec06eb1ec0defdb5e98a1947
Katniss-Nataliia <natko.boiko@gmail.com>
2023-08-01 22:50:43 -0600

    step 2 implemented

--- src/components/phonebook.js
+++ src/components/phonebook.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import { nanoid } from 'nanoid';
 
 export class PhoneBook extends Component {
@@ -6,49 +6,102 @@ export class PhoneBook extends Component {
         super(props);
         this.state = {
             contacts: [],
-            name: ''
-        }
+            filter: '',
+            name: '',
+            number: '',
+        };
     }
 
-    handleChange = (e) => {
-        return this.setState({name: e.target.value})
-    }
-
-    handleSubmit = (e) => {
-        e.preventDefault();
-        const {name} = this.state;
-        const {addContact} = this.props;
-
-        addContact(name); // store the entered name here?
-
-        this.setState({name:""}); //reverse the input to empty line
+    handleChange = e => {
         
+        this.setState({
+            name: e.target.value
+        });
+        
+        
+    };
+    handleNumberChange = e => {
+        this.setState({
+            number: e.target.value
+        })
     }
+
+    handleSubmit = e => {
+        e.preventDefault();
+        const { contacts, name, number } = this.state;
+
+        if (name.trim() && number.trim() !== '') {
+            const newContact = {
+                id: nanoid(),  // Using nanoid() to generate a unique identifier for the new contact
+                name: name,
+                number: number,
+            };
+
+            // Add the new contact to the contacts array and reset the name field
+            this.setState({
+                contacts: [...contacts, newContact],
+                name: '',
+                number: ''
+            });
+        }
+    };
 
     render() {
-        const{name} = this.state;
-        const {handleChange, handleSubmit} = this
+        const { number, name, contacts } = this.state;
+        const { handleChange, handleNumberChange, handleSubmit } = this;
 
         return (
             <div>
+                {/* Input field for entering contact name */}
+                <h2>Name</h2>
                 <input
                     type="text"
                     name="name"
                     pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
                     title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan."
                     required
-                    // value={name}
-                    onChange = {handleChange}
+                    value={name} // Bind the input value to the 'name' state
+                    onChange={handleChange} // Call handleChange function when the input value changes
+                />
+                <h2>Phone Number</h2>
+                <input
+                    type="tel"
+                    name="number"
+                    pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                    title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+                    required
+                    value={number}
+                    onChange={handleNumberChange}
                 />
                 <button onClick={handleSubmit}>Add contact</button>
+
                 <h2>contact</h2>
-                <ul>
-                    <li>{name}</li>
-                </ul>
+
+                <p>Find contacts by name</p>
+                <input
+                type="text"
+                placeholder="Search by name"
+                
+                
+                />
+
+
+
+
+                {contacts.length === 0 ? (
+                    // Display "No contacts" if there are no contacts in the 'contacts' array
+                    <h2>No contacts</h2>
+                ) : (
+                    // Display the list of contacts using the 'ul' and 'li' elements
+                    <ul>
+                        {contacts.map(({name, number, id }) => (
+                           
+                            <li key={id}>{name} : {number}</li>
+                           
+                        ))}
+                    </ul>
+                )}
             </div>
-        )
+        );
     }
-
 }
-
-
